3f0c98b7a27514bbbc983690f7196aeb8a4bf967
Frans701 <franswahyu7@gmail.com>
2022-08-23 23:14:19 +0800

    first commit

---
+++ soal-1.js
@@ -0,0 +1,15 @@
+let kalimat1 = "Andini sangat mencintai kamu selamanya";
+let kalimat2 =
+  "Gunung bromo tak akan mampu menggambarkan besarnya cintaku padamu";
+
+function changeWord(selectedText, changedText, text) {
+  let result;
+  if (kalimat1 === text) {
+    result = kalimat1.replace(selectedText, changedText);
+  } else {
+    result = kalimat2.replace(selectedText, changedText);
+  }
+  return result;
+}
+
+console.log(changeWord("Andini", "Frans", kalimat1));
