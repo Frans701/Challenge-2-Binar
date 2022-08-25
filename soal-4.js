32af0882b69c1b2c7088008024b0469ca47bb67f
Frans701 <franswahyu7@gmail.com>
2022-08-25 09:35:06 +0800

    question number 4, password validation

---
+++ soal-4.js
@@ -0,0 +1,12 @@
+function checkEmail(email) {
+  let regex = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/);
+  let result;
+  if (regex.test(email)) {
+    result = `password ${email} VALID`;
+  } else {
+    result = `password ${email} INVALID`;
+  }
+  return result;
+}
+
+console.log(checkEmail("123456789"));
