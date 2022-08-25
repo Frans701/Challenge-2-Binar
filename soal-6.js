f28b8f7925b5b5b75187e591ea8c4e18192b7770
Frans701 <franswahyu7@gmail.com>
2022-08-26 01:59:49 +0800

    question number 6, largest number 2 in array

---
+++ soal-6.js
@@ -0,0 +1,18 @@
+function getAngkaTerbesarKedua(dataAngka) {
+  let angkaBesar = dataAngka[0];
+  let angkaBesarKedua = 0;
+
+  for (let i = 1; i < dataAngka.length; i++) {
+    if (dataAngka[i] > angkaBesar) {
+      angkaBesarKedua = angkaBesar;
+      angkaBesar = dataAngka[i];
+    } else if (dataAngka[i] > angkaBesarKedua) {
+      angkaBesarKedua = dataAngka[i];
+    }
+  }
+  return angkaBesarKedua;
+}
+
+const dataAngka = [1, 2, 3, 4];
+
+console.log(getAngkaTerbesarKedua());
