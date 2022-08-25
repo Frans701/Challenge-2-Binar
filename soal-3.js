43cb90c6fb44788c288077b1f22941070d6b3329
Frans701 <franswahyu7@gmail.com>
2022-08-25 08:47:22 +0800

    question number 3 about validation email

---
+++ soal-3.js
@@ -0,0 +1,12 @@
+function checkEmail(email) {
+  let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+  let result;
+  if (regex.test(email)) {
+    result = `Alamat ${email} VALID`;
+  } else {
+    result = `Alamat ${email} INVALID`;
+  }
+  return result;
+}
+
+console.log(checkEmail("frans@gmail.co.id"));
